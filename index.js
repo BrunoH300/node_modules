5ae92aa5b0f437cc85002bc2f2b88bc0c4fa75ef
Bruno Henrique <brunoziycabr959@gmail.com>
2023-11-09 15:46:27 -0300

    recebendo parametros na url

--- index.js
+++ index.js
@@ -2,13 +2,18 @@ const { application } = require("express")
 const path = require("path")
 const express = require ("express")
 const app = express()
+
 const caminhoBase = path.join(__dirname, "templates")
 
 app.get('/', (requisicao, resposta) => {
 resposta.sendFile(`${caminhoBase}/index.html`)
 })
 
-
+app.get('/Usuario/: id',(requisicao, resposta) => {
+    const id = requisicao.params.id
+    console.log(`O usuario ${id} esta desaparecido`)
+    resposta.sendFile(`${caminhoBase}/usuarios.html`)
+})
 
 app.listen(3000, () => {
     console.log("servidor rodando na 3000!")
